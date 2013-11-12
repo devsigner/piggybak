51401e57e1607b0a833e687bcd2ee3f01e41b6ef
Samuel Lebeau <samuel.lebeau@gmail.com>
2013-11-12 13:43:24 +0100

    Remove all dependencies to rails_admin & devise gems

--- lib/piggybak/cli.rb
+++ lib/piggybak/cli.rb
@@ -9,16 +9,9 @@ module Piggybak
       if already_installed?
         update
       else        
-        inject_devise
-        inject_rails_admin
         run('bundle install')
         run('rake piggybak_engine:install:migrations')
-        run('rake db:migrate')   
-        run('rails generate devise:install')
-        run('rails generate devise User') 
-        run('rake db:migrate')      
-        run('rails g rails_admin:install')
-        run('rake db:migrate')      
+        run('rake db:migrate')
         mount_piggybak_route
         add_javascript_include_tag
         welcome
@@ -33,19 +26,6 @@ module Piggybak
       say_upgraded
     end
 
-    desc "inject_devise", "add devise"
-    def inject_devise
-      puts 'add reference to devise in GEMFILE'
-      insert_into_file "Gemfile", "gem 'devise'\n", :after => "source 'https://rubygems.org'\n"
-    end
-
-    
-    desc "inject_rails_admin", "add rails_admin"
-    def inject_rails_admin
-      puts 'add reference to rails_admin in GEMFILE'
-      insert_into_file "Gemfile", "gem 'rails_admin'\n", :after => "gem 'devise'\n"
-    end
-  
     desc "mount_piggybak_route", "mount piggybak route"
     def mount_piggybak_route
       insert_into_file "config/routes.rb", "\n  mount Piggybak::Engine => '/checkout', :as => 'piggybak'\n", :after => "Application.routes.draw do\n"
@@ -60,11 +40,6 @@ module Piggybak
     
       insert_into_file 'app/views/layouts/application.html.erb', jit_code_block, :after => "<%= javascript_include_tag \"application\" %>"
     
-    end
-    
-    desc "create user class", "Create a user class"
-    def create_user_class
-      run('rails generate model User')      
     end
     
     desc "welcome", "invite to piggybak"
